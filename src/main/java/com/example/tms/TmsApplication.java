0189e0d31a94e8ec8efc0aee557885e262010717
callme-calm <mritunjayyadav0602@gmail.com>
2024-06-09 18:51:02 +0530

    Basic features

--- src/main/java/com/example/tms/TmsApplication.java
+++ src/main/java/com/example/tms/TmsApplication.java
@@ -1,13 +1,25 @@
 package com.example.tms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TmsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
+			System.out.println("DATABASE_USERNAME: " + System.getenv("DATABASE_USERNAME"));
+			System.out.println("DATABASE_PASSWORD: " + System.getenv("DATABASE_PASSWORD"));
+		};
 	}
 
 }
